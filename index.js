661e0cd1b0c75e0ced2605273abc5e4cc0c28534
Ebo <manduuyla@yahoo.com>
2021-10-17 18:28:17 -0400

    css applied

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ for (let cell of cells) {
     cell.addEventListener('click', () => {
         console.log('cell clicked')
 
+
     })
 }
+const cell = document.querySelector('td')
+const cellAt = cell.getAttribute('')
+console.log()
